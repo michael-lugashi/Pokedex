c51c237500e6594b1c5230984ed90385fcdf25ba
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-10-14 16:55:56 +0300

    Put Request
    a put request goes through when you click on the search button. I use the result of that request to change the Dom to display the results

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 'use strict'
+searchPokemon.addEventListener('click', searching)
+
+async function searching() {
+    console.log(search.value)
+    const response = await axios.get(`https://murmuring-cove-95500.herokuapp.com/api/pokemon/${search.value}`)
+    console.log(response.data.name)
+    // const pokemon = document.getElementById('name')
+    // pokemon.textContent = response.data.name
+    updateDom(response.data)
+
+}
+
+function updateDom(data) {
+    // const pokemon = document.getElementById('name')
+    namePokemon.textContent = `Name: ${data.name}`
+    height.textContent = `Height: ${data.height}`
+    weight.textContent = `Weight: ${data.weight}`
+    type.textContent = `Types: ${data.types}`
+    pokemonPicture.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${data.id}.png`
+}
