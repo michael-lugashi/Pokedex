ee1e5f4eaf0e7adab092c24deb60407c65fc89ad
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-10-16 20:35:16 +0300

    Type List
    I made it so that when I click on a type all the Pokémon of that type appear. When I click on one of the Pokémon it searches that Pokémon.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 'use strict'
 searchPokemon.addEventListener('click', searching)
-types.addEventListener('click', findPokemon) 
-// catchPokemon.addEventListener('click', changeCatchStatus.bind(event, 'ignor','helo'))
-async function searching() {
-    let done = await axios.get(`https://pokeapi.co/api/v2/type/normal`)
-    console.log(done)
+types.addEventListener('click', findPokemonByType)
+pokemonTypeList.addEventListener('click', searchPokemonFromType)
+
+function searching() {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${search.value}`)
     .then(response => updateDom(response.data))
-    .catch(err=> alert(err))
-    // let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${search.value}`, {method: 'GET'})
-    // response = JSON.parse(response)
-
+    .catch(alert('That Pokemon does not exist!'))
 }
 
 function updateDom(data) {
-    // let catchOrRelease = await getCatchStatus(`https://murmuring-cove-95500.herokuapp.com/api/collection/status/${data.id}`)
-    // catchPokemon.addEventListener('click', changeCatchStatus.bind(event, `https://murmuring-cove-95500.herokuapp.com/api/collection/status/${data.id}`))
-    // catchPokemon.textContent=catchOrRelease
-    // catchPokemon.hidden=false;
     namePokemon.textContent = `Name: ${data.name}`
     height.textContent = `Height: ${data.height}`
     weight.textContent = `Weight: ${data.weight}`
@@ -43,27 +35,45 @@ function updatePicture(id) {
 
 }
 
-// async function getCatchStatus(url) {
-//     const response = await axios.get(url)
-//     return response.data.status
-// }
-
-// async function changeCatchStatus(url) {
-//     const res = await axios.post(url, {status: 'caught'})
-//     console.log(res)
-// }
-
 function createTypeEl(text) {
     const type = document.createElement('span')
     type.textContent = text
     return type
 }
 
-function findPokemon(event) {
+function findPokemonByType(event) {
     if (event.target.tagName !== 'SPAN') {
         return
     }
     axios.get(`https://pokeapi.co/api/v2/type/${event.target.textContent}`)
     .then(result=>displayNames(result.data.pokemon))
+}
 
+function displayNames(pokemon) {
+    let first = pokemonTypeList.firstElementChild;
+    while (first) {
+        first.remove();
+        first = pokemonTypeList.firstElementChild;
+    }
+    for (const poki of pokemon) {
+        console.log(poki.pokemon.name)
+        pokemonTypeList.append(pokemonName(poki.pokemon.name))
+    }
+
+}
+
+
+
+
+function pokemonName(name) {
+    const pokemon = document.createElement('li')
+    pokemon.textContent = name
+    return pokemon
+}
+function searchPokemonFromType(event){
+    if (event.target.tagName !== 'LI') {
+        return
+    }
+    search.value = event.target.textContent
+    searching()
 }
