1b88f23bdfc9cf204f64ad07ddbfbb0ffbf40f57
Aviv Yamin <aviv912@gmail.com>
2021-10-23 18:30:31 +0300

    Aviv: I updated my files to even up

--- AvivsBack/src/routers/pokemonRouter.js
+++ AvivsBack/src/routers/pokemonRouter.js
@@ -1,25 +1,13 @@
 // GET - /pokemon/get/:id - should respond with a pokemon queried by its id using Route parameters
 // GET - /pokemon/query - should respond with a pokemon queried by its name using a JSON send in the requests query: { query: <string> }
-const { json } = require('body-parser');
 const express = require('express');
 const router = express.Router();
 
-router.get('/pokemon/get/:id', function(req, res) {
+router.get('/:id', (req, res) => {
     const pokemonId = req.params.id;
     P.getPokemonByName(pokemonId).then((pokemon) => {
         res.send(pokemon);
     })
 })
 
-
-// router.get('/pokemon/query', function(req, res) {
-//     const pokemonName = req.query;
-//     const pokemon = P.getPokemonByName(pokemonName).then((pokemon) => {
-        
-//        const { name, height, weight, types, front_pic, back_pic, abilities } = pokemon;
-        
-   
-//     json.send({
-        
-//     })
-//   })
+module.exports = router;
