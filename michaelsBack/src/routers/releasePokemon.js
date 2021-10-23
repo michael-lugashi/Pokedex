eb10f3b66baddf47d49e1925071f43fabe18914f
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-10-23 20:45:27 +0300

    Michael - release Pokemon
    I release Pokémon by deleting the file

--- michaelsBack/src/routers/releasePokemon.js
+++ michaelsBack/src/routers/releasePokemon.js
@@ -7,11 +7,31 @@ const fs = require('fs');
 const os = require('os');
 
 router.delete('/:id', (req, res) => {
-  console.log(req.params.id);
-  P.getPokemonByName(req.params.id).then((pokemon) => {
-    // console.log(pokemon)
-    res.send(pokemon);
+    console.log(req.params.id);
+    fs.unlink(
+      `./src/users/${os.userInfo().username}/${req.params.id}.json`,
+      (err) => {
+        if (err) {
+          res.status(403).send('That pokemon has not been caught yet');
+          return;
+        }
+        res.send('delete succesful')
+        // if no error, file has been deleted successfully
+        console.log('File deleted!');
+      }
+    );
+    //   fs.existsSync(`./src/users/${os.userInfo().username}/${req.params.id}.json`, (exists)=>{
+    //       if (!exists) {
+    //           res.status(403).send('That pokemon has not been caught yet')
+    //           return
+    //       }
+  
+    //   });
+    //   P.getPokemonByName(req.params.id).then((pokemon) => {
+    //     // console.log(pokemon)
+  
+    //     res.send(pokemon);
+    //   });
   });
-});
-
-module.exports = router;
+  
+  module.exports = router;
